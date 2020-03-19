b49d87d4b72a6419ddb03c5fef770cdc494d799c
Juan Madurga <jlmadurga@gmail.com>
2020-03-19 17:53:00 +0100

    update license

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[],
-    license="MIT",
+    license="BSD",
     zip_safe=False,
     keywords='django-ontruck',
     classifiers=[
