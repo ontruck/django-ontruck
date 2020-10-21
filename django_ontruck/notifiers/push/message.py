c997d3506fd40087903865803cacf9bc121e9f5f
Moisès Macià <mmacia@gmail.com>
2020-10-21 15:15:06 +0200

    remove category data

--- django_ontruck/notifiers/push/message.py
+++ django_ontruck/notifiers/push/message.py
@@ -1,8 +1,8 @@
+from abc import ABC, abstractmethod
 from .device import Device
-from .category import Category
 
 
-class Message(object):
+class Message(ABC):
     __slots__ = ('device_type', 'category', 'message',)
 
     def __init__(self, device_type, category, message=None):
@@ -19,15 +19,9 @@ class Message(object):
         return None
 
     @property
+    @abstractmethod
     def extra(self):
-        if self.device_type == Device.ANDROID:
-            return self._android_extra()
-        elif self.device_type == Device.IOS:
-            return self._ios_extra()
-        elif self.device_type == Device.WEB:
-            return self._web_extra()
-        else:
-            raise RuntimeError('invalid device type')
+        raise NotImplementedError()
 
     def to_dict(self):
         if self.title and self.body:
@@ -39,35 +33,3 @@ class Message(object):
                 d['use_fcm_notifications'] = False
 
             return d
-
-    def _android_extra(self):
-        extra = {'priority': 'high', 'category': self.category.value}
-
-        if self.message and 'title' in self.message and 'body' in self.message:
-            extra = {**extra, **self.message}
-
-        return extra
-
-    def _ios_extra(self):
-        aps = {'category': self.category.value}
-
-        if self.category == Category.PROFILE_UPDATED:
-            aps['content-available'] = 1
-            return aps
-
-        aps['alert'] = self.message
-        aps['sound'] = Category.phone_sound_for(self.category)
-        aps['mutable-content'] = 1
-
-        if self.category == Category.OFFER:
-            aps['thread-id'] = 'offers-group'
-
-        return {'aps': aps}
-
-    def _web_extra(self):
-        extra = {'category': self.category.value}
-
-        if self.message and 'title' in self.message and 'body' in self.message:
-            extra = {**extra, **self.message}
-
-        return extra
