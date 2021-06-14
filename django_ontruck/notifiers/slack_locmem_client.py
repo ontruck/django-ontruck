1e28af7d9e549d53ad8d001a10a0f36393e7ca36
Moisès Macià <mmacia@gmail.com>
2021-06-14 14:08:25 +0200

    remove concurrent worker code

--- django_ontruck/notifiers/slack_locmem_client.py
+++ django_ontruck/notifiers/slack_locmem_client.py
@@ -5,16 +5,12 @@ logger = logging.getLogger(__name__)
 
 
 class SlackLocMemClient(object):
-    concurrent_worker = 'master'
-
     class SlackLocMemApi(object):
-
         def post(self, *args, **kwargs):
-            caller = getattr(module, f'slack_outbox_api_{SlackLocMemClient.concurrent_worker}')
+            caller = getattr(module, 'slack_outbox_api')
             caller.append({'args': args, 'kwargs': kwargs})
 
     class SlackLocMemChat(object):
-
         def post_message(self, *args, **kwargs):
             channel = args[0]
             message = args[1] if len(args) > 1 else kwargs.get('text')
@@ -22,15 +18,15 @@ class SlackLocMemClient(object):
             options.pop('text', None)
             logger.info("SLACK CHAT: %s\n%s\nkwargs = %s", channel, message, options)
 
-            caller = getattr(module, f'slack_outbox_chat_{SlackLocMemClient.concurrent_worker}')
+            caller = getattr(module, 'slack_outbox_chat')
             caller.append({'args': args, 'kwargs': kwargs})
 
     def __init__(self, *args, **kwargs):
         self._api = self.SlackLocMemApi()
         self._chat = self.SlackLocMemChat()
 
-        api_attr = f'slack_outbox_api_{SlackLocMemClient.concurrent_worker}'
-        chat_attr = f'slack_outbox_chat_{SlackLocMemClient.concurrent_worker}'
+        api_attr = 'slack_outbox_api'
+        chat_attr = 'slack_outbox_chat'
 
         if not hasattr(module, api_attr):
             setattr(module, api_attr, [])
